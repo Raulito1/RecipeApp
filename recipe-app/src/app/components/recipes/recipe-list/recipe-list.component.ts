159719fabb42dbf6b0ac10267c432758a4d0469a
Raulito <ralvaradojr399@gmail.com>
2021-09-07 20:24:27 -0400

    Added Html & CSS to my Components Also Created Model Files For Recipe And Ingredients

--- recipe-app/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[]= [
+    new Recipe('Test Name', 'Test Description','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7QCcOwqOwMPbBpgcUhc6KJ8Zjv0YAjJNL7w&usqp=CAU')
+  ];
 
   constructor() { }
 
